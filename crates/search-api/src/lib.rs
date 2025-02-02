1c50050a8e683af6bc601281556898423d6eaee2
Renning Bruns <ren@renmail.net>
2025-02-01 16:09:13 -0800

    set search api

--- crates/search-api/src/lib.rs
+++ crates/search-api/src/lib.rs
@@ -1,14 +1,63 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use once_cell::sync::OnceCell;
+use std::error::Error as StdError;
+
+#[derive(Debug)]
+pub struct SearchResult {
+    pub title: String,
+    pub url: String,
+    pub snippet: Snippet
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub struct Snippet {
+    pub content: String,
+    pub highlighted: Vec<(usize, usize)>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug)]
+pub struct SearchResults {
+    pub results: Vec<SearchResult>,
+}
+
+#[derive(Debug)]
+pub enum SearchError {
+    Internal(String),
+}
+
+impl std::fmt::Display for SearchError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SearchError::Internal(msg) => write!(f, "Search error: {}", msg),
+        }
+    }
+}
+
+impl From<Box<dyn StdError>> for SearchError {
+    fn from(err: Box<dyn StdError>) -> Self {
+        SearchError::Internal(err.to_string())
+    }
+}
+
+impl StdError for SearchError {}
+
+pub trait Search: Send + Sync {
+    fn search(&self, query: &str) -> Result<SearchResults, SearchError>;
+}
+
+static GLOBAL_SEARCH_IMPL: OnceCell<Box<dyn Search>> = OnceCell::new();
+
+pub fn set_search_impl(backend: Box<dyn Search>) -> Result<(), Box<dyn StdError>> {
+    GLOBAL_SEARCH_IMPL
+        .set(backend)
+        .map_err(|_| "Search implementation is already set!".into())
+}
+
+pub fn search(query: &str) -> Result<SearchResults, SearchError> {
+    if let Some(backend) = GLOBAL_SEARCH_IMPL.get() {
+        backend.search(query)
+    } else {
+        Err(SearchError::Internal(
+            "No search implementation set!".to_string(),
+        ))
     }
 }
